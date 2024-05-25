3bc65125712ca6618d5e7b30d67c79b15d790b1c
Ismael Cerezo <Ismaelito91>
2024-05-25 13:13:39 +0200

    Supprime le dossier back

--- back/routes/articleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const articleController = require("../controllers/articleController.js");
-
-// Route for retrieving articles
-router.get("/", articleController.getArticles);
-
-module.exports = router;
